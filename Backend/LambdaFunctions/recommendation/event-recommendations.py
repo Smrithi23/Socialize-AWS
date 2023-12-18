d2f39a663bf37d7befcec65e39ae00885d21cf50
varsha-mkanmuri <124398391+varsha-mkanmuri@users.noreply.github.com>
2023-12-17 23:41:58 -0500

    Update event-recommendations.py

--- Backend/LambdaFunctions/recommendation/event-recommendations.py
+++ Backend/LambdaFunctions/recommendation/event-recommendations.py
@@ -9,18 +9,11 @@ table = dynamodb.Table('activity-details')
 def lambda_handler(event, context):
     # TODO implement
     user_id = event['user_id']
-    id_mapping = {
-     'mc5470': '20738801',
-     'sp4135': '225139423',
-     'sd3596': '7137746',
-     'vk2497': '7333165',
-     'vm8901': '9918706'
-    }
     
     response = client.get_recommendations(
         campaignArn='arn:aws:personalize:us-east-1:252225289072:campaign/campaign-1',
         filterArn='arn:aws:personalize:us-east-1:252225289072:filter/filter-event',
-        userId=id_mapping[user_id],
+        userId= user_id,
         numResults=10)
         
     itemList = response['itemList']
